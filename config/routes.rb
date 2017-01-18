ab43d52b09884fbf813b8de08b1ebc891592596e
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-01-18 16:41:41 +0100

    add user registration, test failed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :ping, only: [:index], constraints: { format: 'json' }
-    end
-  end
+
+     namespace :v1 do
+         mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+       end
+     end
+   end
 end
