c0b04035be965a321a3dcdd7ac62ecb3012de02a
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-01-19 13:49:22 +0100

    add performance data controller, test failed

--- config/application.rb
+++ config/application.rb
@@ -18,11 +18,14 @@ Bundler.require(*Rails.groups)
 
 module CooperApi
   class Application < Rails::Application
+    # [...]
     config.middleware.insert_before 0, Rack::Cors do
-        allow do
-          origins '*'
-          resource '*', headers: :any, methods: [:get, :post, :put, :delete]
-        end
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete],
+                      expose: %w(access-token expiry token-type uid client),
+                      max_age: 0
       end
     end
   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
+   namespace :v0 do
     namespace :v1 do
       resources :ping, only: [:index], constraints: { format: 'json' }
          mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
-           resources :performance_data, only: [:create]
+           resources :performance_data, only: [:create ]
+             resources :performance_data, only: [:create, :index]
      end
    end
+  end
 end
